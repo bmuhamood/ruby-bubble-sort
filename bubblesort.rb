8382e565e44736c632d68c13923fd6f468aca801
bmuhamood <bbosa2009@gmail.com>
2020-08-27 18:49:34 +0400

    Added linters files to github folder

--- bubblesort.rb
+++ bubblesort.rb
@@ -4,17 +4,22 @@ def bubble_sort(array)
     loop do
         swapped = false
         (n-1).times do |num|
+
+            # creating condition
             if array[num] > array[num+1]
+            # swapping elements when the next value either true of false
                 array[num], array[num + 1] = array[num + 1], array[num]
                 swapped = true
+                array
             end
     end
 
     break if not swapped
         
     end
+    # return array
     array
 end
 
-b = [4,3,78,2,0,2]
-a = bubble_sort(b)
+a = [4,3,78,2,0,2]
+p bubble_sort(a)
