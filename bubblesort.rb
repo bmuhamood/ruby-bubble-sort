11fc5d8dc664882a4e637e3167372f9894ff111e
bmuhamood <bbosa2009@gmail.com>
2020-08-28 01:22:08 +0400

    linters

--- bubblesort.rb
+++ bubblesort.rb
@@ -2,14 +2,13 @@ def bubble_sort(array)
   myarr = array.length
   loop do
     swapped = false
-  (myarr - 1).times do | arr |
-  if array[arr] > array[arr +1]
-  array[arr], array[arr + 1] = array[arr + 1], array[arr]
-  swapped = true
-  array
-  end
-  end
-  break if !swapped
+    (myarr-1).times do |arr|
+            if array[arr] > array[arr +1]
+            array[arr], array[arr + 1] = array[arr + 1], array[arr]
+            swapped = true
+          end
+        end
+    break if !swapped
   end
   array
 end
--- str.rb
+++ str.rb
@@ -1,9 +1,9 @@
 def bubble_sort(vals)
     n = vals.length
   loop do
-    swapped = false
-  (n - 1).times do | mystr |
-  if vals[mystr] < vals[mystr + 1]
+  swapped = false
+  (n-1).times do |mystr|
+    if vals[mystr] < vals[mystr + 1]
   vals[mystr], vals[mystr + 1] = vals[mystr + 1], vals[mystr]
   swapped = true
   vals
@@ -13,5 +13,5 @@ def bubble_sort(vals)
   end
   vals
 end
-  string = ["hi", "hello", "hey"]to_s
+  string = ["hi", "hello", "hey"]
   print bubble_sort(string)
