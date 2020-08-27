2aa1b42397e5024fb76f2ba339e02e5882bfbc08
bmuhamood <bbosa2009@gmail.com>
2020-08-27 23:19:20 +0400

    fixing linters

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,21 +1,22 @@
 def bubble_sort(array)
-    # length of the array
-    myarr = array.length
-    loop do
-      swapped = false
-      (myarr - 1).times do |arr |
-        # condition
-      if array[arr] > array[arr + 1]
-    # swapping elements when the next value either true of false
-    array[arr], array[arr + 1] = array[arr + 1], array[arr]
-    swapped = true
-    array
-    end
-    end
-    break if not swapped
-    end
-    return array
-    array
-    end
-    a = [4, 3, 78, 2, 0, 2]
-    p bubble_sort(a)
+# length of the array
+myarr = array.length
+loop do
+  swapped = false
+  (myarr - 1).times do |arr |
+    # condition
+  if array[arr] > array[arr + 1]
+# swapping elements when the next value either true of false
+array[arr], array[arr + 1] = array[arr + 1], array[arr]
+swapped = true
+array
+end
+end
+break if not swapped
+end
+#
+return array
+array
+end
+a = [4, 3, 78, 2, 0, 2]
+p bubble_sort(a)
